fe31e0cbc0cc7621a0759bb9d0f837efd2b30abf
ellepagel120 <60626570+ellepagel120@users.noreply.github.com>
2020-04-18 22:29:12 -0500

    Add files via upload

---
+++ js/surprise.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+
+
+	$(".shape1").click(function() {
+
+		$(".shape1").addClass("animate");
+		$(".shape1").css({"background":"url(images/Splatter-01.png"});
+
+		});
+});
